3d620d23c38a6103560038e4bb1312d6df29cb44
sorrybro55555 <zom_bie1596@hotmail.com>
2020-01-24 17:27:06 +0700

    LAB8

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main(){	
+	float money,interest,amount,payment,i,b,n;
+	
+	cout << "Enter intitial loan: ";
+	cin >> money;
+	cout << "Enter interest rate per year (%): ";
+	cin >> interest;
+	cout << "Enter amount you can pay per year: ";
+	cin >> amount;
+
+	float total = money+(money/100)*interest;
+	n = money;
+	int year = 1;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -10,21 +22,36 @@ int main(){
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
-	cout << setw(13) << left << "Total";
+	cout << setw(13) << left << "Total";	
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	while(n > 0){
+		cout << fixed << setprecision(2); 
+		cout << setw(13) << left << year; 
+		cout << setw(13) << left << money;
+		cout << setw(13) << left << (money/100)*interest;
+		cout << setw(13) << left << total;
+		
+		if(total >= amount){
+			payment = amount;
+		
+		}else{
+		payment = total;
+		}
+		cout << setw(13) << left << payment;
+		n = total - payment;
+		money = n;
+		cout << setw(13) << left << n;
+		cout << "\n";	
+		year ++;
+		total = money+(money/100)*interest;
+	} 
+		
+	
 	
 	return 0;
 }
--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+void printO(int N,int M){
+	if(N<=0 or M<=0){
+		cout <<"Invalid input ";
+	}
+	else{
+		for(int j = 0;j < N; j++ ){
+			for(int i = 0;i < M; i++){
+				cout << 'O';
+			}
+			cout << "\n";
+		}
+	}
+
+		
+	
+}
 
 int main(){
 	
